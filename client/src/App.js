4b6f84b196f763888e9cba20910d4419a7a69e2b
Darrell Carter <darrellcarter22@gmail.com>
2021-11-13 16:23:08 -0500

    created app, and folder structure, starting on aboutme page

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import About from "./PortfolioContainer/components/About/profile";
+
+// import Header from "./PortfolioContainer/components/Header";
+import Footer from "./PortfolioContainer/components/Footer";
+
+function App() {
+    return (
+        <div>
+
+        
+      <About/>
+        <Footer/>
+
+        </div>
+        );
+  }
+
+export default App;
