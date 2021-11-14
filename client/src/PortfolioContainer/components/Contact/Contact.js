8019a79a5f7a24718170fc3320ae7c04f7770c90
Darrell Carter <darrellcarter22@gmail.com>
2021-11-14 16:33:39 -0500

    updated resume and folder structure and contact page

--- client/src/PortfolioContainer/components/Contact/Contact.js
+++ client/src/PortfolioContainer/components/Contact/Contact.js
@@ -1,10 +1,39 @@
-import React from "react";
-import  "./contact.css"
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-function Contact() {
+
+function Contact (){
+
   return (
-      <h1>Contact page</h1>
-  )
+    <Form>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>Name</Form.Label>
+      <Form.Control type="text" placeholder="Enter name" />
+      <Form.Text className="text-muted">
+        Please enter name.
+      </Form.Text>
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
+
+    <div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label"></label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+    <Button variant="primary" type="submit">
+      Submit
+    </Button>
+  </Form>
+  );
 }
+
 
 export default Contact;
